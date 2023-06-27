a42f3562111993929c53ef16d1b56cca13318a28
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-06-27 16:09:41 +0530

    updated app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import AppContextProvider from "@/contexts/AppContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import AuthLayout from "@/layout/AuthLayout";
 
 const activeChain = ChainId.Goerli;
 
@@ -9,7 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContextProvider>
       <ThirdwebProvider activeChain={activeChain} autoConnect>
-        <Component {...pageProps} />
+        <AuthLayout>
+          <Component {...pageProps} />
+        </AuthLayout>
       </ThirdwebProvider>
     </AppContextProvider>
   );
