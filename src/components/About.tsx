5d0fde2533b8648382006bce1859aab029e2faa7
rahulav35 <rahulmohanav35@gmail.com>
2023-07-10 16:16:53 +0530

    overflowFixed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='lg:h-[500px] h-[700px] w-[90%]  flex lg:flex-row flex-col justify-between items-center  mb-12 lg:mb-1  '>
+    <div className='lg:h-[500px] h-[700px] lg:w-[90%] w-[80%]  flex lg:flex-row flex-col justify-between items-center  mb-10 lg:mb-1  '>
         <div className="lg:h-[100%] h-[55%] lg:w-[55%]  flex flex-col justify-start lg:items-start items-center lg:pl-10 box-border">
         <h1 className="lg:text-[1.7rem] text-[1.5rem] tracking-wider text-[#A96500] font-MuseoModerno font-semibold my-5 lg:my-10 ">About In&O</h1>
-        <p className='lg:text-[1.2rem] text-[1.1rem]  text-[#000000a8] font-sans lg:max-w-[80%] max-w-[95%] lg:text-start text-center tracking-wider'>In&O is an ecommerce platform that specializes in providing custom printed design t-shirts. With In&O, customers have the opportunity to create unique and personalized t-shirts by selecting from a wide range of design options or uploading their own artwork. The platform offers a user-friendly interface, making it easy for customers to customize their t-shirts with different colors, fonts, and graphics.</p>
+        <p className='lg:text-[1.2rem] text-[1.1rem]  text-[#000000a8] font-sans lg:max-w-[80%] max-w-[99%] lg:text-start text-center tracking-wider'>In&O is an ecommerce platform that specializes in providing custom printed design t-shirts. With In&O, customers have the opportunity to create unique and personalized t-shirts by selecting from a wide range of design options or uploading their own artwork. The platform offers a user-friendly interface, making it easy for customers to customize their t-shirts with different colors, fonts, and graphics.</p>
         </div>
 
-        <div className="lg:h-[85%] h-[45%] lg:w-[50%]   flex lg:items-start items-center justify-start pl-[5%] box-border lg:mt-0 mt-10">
-          <div style={{backgroundImage:'url(/svg/T5.svg)'}} className="lg:w-[60%] lg:h-[80%] h-[85%] rounded-lg bg-cover bg-center bg-no-repeat flex items-center justify-center">
+        <div className="lg:h-[85%] h-[45%] lg:w-[50%] w-[90%]  flex lg:items-start items-center justify-start pl-[5%] box-border lg:mt-0 mt-16">
+          <div style={{backgroundImage:'url(/svg/T5.svg)'}} className="lg:w-[60%] lg:h-[80%] h-[85%] w-[80%] rounded-lg bg-cover bg-center bg-no-repeat flex items-center justify-center">
             <img src="/svg/T5.svg" alt="" className="w-full h-full rounded-lg rotate-12" />
           </div>
         </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="min-h-[30vh] w-[100%] flex lg:flex-row flex-col items-center justify-between lg:px-10 py-5 box-border text-white bg-black bottom-0 relative">
+    <div className="lg:min-h-[30vh] h-[45vh] w-[100%] flex lg:flex-row flex-col items-center justify-between lg:px-10 py-5 box-border text-white bg-black bottom-0 relative">
       <div className="lg:min-h-[45px] h-[100px] w-[30%] lg:w-[12%] lg:ml-10">
         {" "}
         <img className="h-[100%] w-[100%]" src="/svg/In&O.svg" alt="" />
       </div>{" "}
-      <div className="lg:h-[80%] h-[60%] lg:w-[25%]  grid lg:grid-cols-2 lg:gap-2 grid-cols-1 gap-2 lg:ml-0 ml-5">
+      <div className="lg:h-[80%] h-[60%] lg:w-[25%] flex flex-col items-center justify-evenly  lg:grid lg:grid-cols-2 lg:gap-2  lg:ml-0 ml-5">
         <h1 className="text-sm font-medium ">About Us</h1>
         <h1 className="text-sm font-medium ">Instagrm</h1>
       
