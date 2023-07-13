c20b9982952781ec23851adada937eaaecee20e0
rahulav35 <rahulmohanav35@gmail.com>
2023-07-13 21:08:49 +0530

    mobileviewadded

--- src/components/OrderPlaced.tsx
+++ src/components/OrderPlaced.tsx
@@ -11,8 +11,8 @@ function OrderPlaced() {
     <div className="h-screen w-screen flex flex-col items-center justify-start fixed top-0  ">
       <Header />
       <div className="h-full w-full  flex items-center justify-center fixed top-0 rounded-lg overflow-hidden bg-transparent backdrop-blur-[2px] z-[100]">
-        <div className="h-[45%] w-[40%] flex bg-white flex-col items-center justify-start bg-SuccessPopper rounded-lg shadow-2xl bg-cover  ">
-          <img src="/gif/animation2.gif" alt="" className="h-[130px] w-[130px] mt-7" />
+        <div className="lg:h-[45%] h-[35%] lg:w-[40%] w-[80%] flex bg-white flex-col items-center justify-start bg-SuccessPopper rounded-lg shadow-2xl bg-cover  ">
+          <img src="/gif/animation2.gif" alt="" className="h-[130px] w-[130px] lg:mt-7" />
           <span className="text-[2.0rem] text-black font-extrabold -mt-3">
             Order Placed
           </span>
