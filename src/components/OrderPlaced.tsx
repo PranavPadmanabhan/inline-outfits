a6fd37f0a8518f1ae02848b9d32792328005dd51
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-07-13 17:05:07 +0530

    updated givenAddress

--- src/components/OrderPlaced.tsx
+++ src/components/OrderPlaced.tsx
@@ -10,10 +10,10 @@ function OrderPlaced() {
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-start fixed top-0  ">
       <Header />
-      <div className="h-full w-full  flex items-center justify-center fixed top-0 bg-transparent backdrop-blur-[2px] z-[100]">
-        <div className="h-[45%] w-[40%] flex  flex-col items-center justify-center bg-SuccessPopper bg-cover  ">
-          <img src="/svg/home.svg" alt="" className="h-[70px] w-[70px]" />
-          <span className="text-[2.0rem] text-black font-extrabold mt-2">
+      <div className="h-full w-full  flex items-center justify-center fixed top-0 rounded-lg overflow-hidden bg-transparent backdrop-blur-[2px] z-[100]">
+        <div className="h-[45%] w-[40%] flex bg-white flex-col items-center justify-start bg-SuccessPopper rounded-lg shadow-2xl bg-cover  ">
+          <img src="/gif/animation2.gif" alt="" className="h-[130px] w-[130px] mt-7" />
+          <span className="text-[2.0rem] text-black font-extrabold -mt-3">
             Order Placed
           </span>
           <button
@@ -21,7 +21,7 @@ function OrderPlaced() {
               setOrderPlaced(false);
               router.push("/shop");
             }}
-            className=" h-[40px] w-[130px] text-[0.8rem] text-white bg-black flex items-center justify-center rounded-md mt-5"
+            className=" h-[40px] w-[130px] text-[0.8rem] text-white bg-black flex items-center justify-center rounded-md mt-2"
           >
             Continue Shopping
           </button>
